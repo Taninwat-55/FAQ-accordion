890d7689f048e60c2494eb797a9ed7aa6b7288fc
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2024-08-15 18:52:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const accordionBtns = document.querySelectorAll(".accordion-btn");
+
+accordionBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    this.classList.toggle("active");
+    const accordionAnswer = this.nextElementSibling;
+    const plusIcon = this.querySelector(".plus-icon");
+    const minusIcon = this.querySelector(".minus-icon");
+
+    if (accordionAnswer.style.maxHeight) {
+      accordionAnswer.style.maxHeight = null;
+      plusIcon.style.display = "block";
+      minusIcon.style.display = "none";
+    } else {
+      accordionAnswer.style.maxHeight = accordionAnswer.scrollHeight + "px";
+      plusIcon.style.display = "none";
+      minusIcon.style.display = "none";
+    }
+  });
+});
